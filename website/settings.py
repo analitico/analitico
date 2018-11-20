81d102aa6a361a50e508d476a2d11b8e4dc875ad
Gionata Mettifogo <gmettifogo@gmail.com>
2018-11-20 14:24:04 +0100

    Documentation link

--- website/settings.py
+++ website/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+# https://dev.mysql.com/doc/connector-python/en/connector-python-django-backend.html
 
 DATABASES = {
     'default': {
