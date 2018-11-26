37e0ccdcf761bfa56bb40e5280d4c47782f8c443
Gionata Mettifogo <gmettifogo@gmail.com>
2018-11-26 10:47:13 +0100

    Made easier to copy & paste in blocks

--- conf/setup.sh
+++ conf/setup.sh
@@ -1,47 +1,56 @@
 
-echo 'Create User'
+echo 'Creating User'
 adduser www
 usermod -aG sudo www
 su www
 
-echo 'Set Server Name'
+echo 'Setting Server Name'
 sudo nano /etc/hostname /etc/hosts
+echo 'done'
 
-echo 'Update Ubuntu'
+echo 'Updating Ubuntu'
 sudo apt update && sudo apt upgrade -y
 sudo apt install git -y
+echo 'done'
 
-echo 'Update Python'
+echo 'Updating Python'
 sudo apt install python-pip -y
 sudo apt install python3.6-dev -y
 sudo apt install python3-venv -y
 sudo apt install build-essential -y
 sudo apt install python3-dev -y
 sudo apt install python3-venv -y
+echo 'done'
 
-echo 'Download Analitico'
+echo 'Downloading Analitico'
 cd /home/www
 git clone https://gitlab.com/analitico/analitico.git
 cd /home/www/analitico
+echo 'done'
 
 echo 'Create Virtual Environment'
 cd /home/www/analitico
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+echo 'done'
 
-echo 'Install Nginx'
+
+echo 'Installing Nginx'
 sudo apt install nginx -y
 systemctl status nginx
+echo 'done'
 
-echo 'Apply Configuration'
+echo 'Applying Configuration'
 sudo rm /etc/nginx/nginx.conf
 sudo ln -s /home/www/analitico/conf/nginx.conf /etc/nginx/
 sudo systemctl restart nginx
+echo 'done'
 
-echo 'Configure Gunicorn'
+echo 'Configuring Gunicorn'
 cd /home/www/analitico/source
 python3 manage.py collectstatic
 sudo ln -s /home/www/analitico/conf/gunicorn_analitico.service /etc/systemd/system
 sudo systemctl start gunicorn_analitico
 systemctl status gunicorn_analitico
+echo 'done'
