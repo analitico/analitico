8cb2ce5922207f340d696c2fd928702d3108778c
Gionata Mettifogo <gmettifogo@gmail.com>
2018-12-08 09:36:51 +0100

    Name fixes

--- source/api/test/test_api_project.py
+++ source/api/test/test_api_project.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from api.models import Project
 
+# pylint: disable=no-member
+
 PRJ_SETTINGS = {
     'data': {
         'parameters': {
@@ -17,13 +19,13 @@ class ProjectsApiTests(APITestCase):
     def setUp(self):
         Project.objects.create(id='s24-order-time', settings=PRJ_SETTINGS)
 
-    def test_prj_exists(self):
+    def test_project_model_with_existing_project(self):
         prj1 = Project.objects.get(pk='s24-order-time')
         self.assertIsNotNone(prj1)
         self.assertEqual(prj1.settings['data']['parameters']['learning_rate'], 0.50)
         self.assertEqual(prj1.settings['data']['parameters']['iterations'], 30)
 
-    def test_api_prj_exists(self):
+    def test_project_api_with_existing_project(self):
         response = self.client.get('/api/v1/project/s24-order-time/', format='json')
         self.assertIsNotNone(response.data)
         settings = response.data['data']['settings']['data']
@@ -31,7 +33,7 @@ class ProjectsApiTests(APITestCase):
         self.assertEqual(settings['parameters']['learning_rate'], 0.50)
         self.assertEqual(settings['parameters']['iterations'], 30)
 
-    def test_api_project_missing(self):
+    def test_project_api_with_missing_project(self):
         response = self.client.get('/api/v1/project/fake-id/', format='json')
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.reason_phrase, 'Not Found')
