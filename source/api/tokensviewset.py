bcf85d61c86ac909a1c9b85507ca591c64bff71e
Gionata Mettifogo <gmettifogo@gmail.com>
2018-12-14 14:34:24 +0100

    Implemented more token methods

--- source/api/tokensviewset.py
+++ source/api/tokensviewset.py
@@ -4,9 +4,10 @@
 
 from django.shortcuts import get_object_or_404
 
-from rest_framework import viewsets
-from rest_framework import serializers
 from rest_framework import exceptions
+from rest_framework import status
+from rest_framework import serializers
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
@@ -22,7 +23,17 @@ class TokenSerializer(serializers.Serializer):
 
 
 class TokenViewSet(viewsets.ViewSet):
-    """ A ViewSet to list, detail, create, update and delete API auth tokens. """
+    """ List, detail, create, update and delete API authorization tokens. """
+
+    def _tokens(self, request):
+        """ Returns only tokens belonging to the authenticated user """
+        return Token.objects.filter(user=request.user)
+
+    def _token(self, request, pk):
+        """ Returns a specific token (as long as it belongs to authenticated user) """
+        return get_object_or_404(self._tokens(request), pk=pk)
+
+    # inherited
 
     def get_permissions(self):
         """ The user needs to be authenticated in order to manage his own tokens """
@@ -30,15 +41,12 @@ class TokenViewSet(viewsets.ViewSet):
 
     def list(self, request):
         """ Returns tokens belonging to the authenticated user """
-        queryset = Token.objects.filter(user=request.user)
-        serializer = TokenSerializer(queryset, many=True)
+        serializer = TokenSerializer(self._tokens(request), many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         """ Returns a specific token by key """
-        queryset = Token.objects.filter(user=request.user)
-        token = get_object_or_404(queryset, pk=pk)
-        serializer = TokenSerializer(token)
+        serializer = TokenSerializer(self._token(request, pk))
         return Response(serializer.data)
 
     def create(self, validated_data):
@@ -57,3 +65,20 @@ class TokenViewSet(viewsets.ViewSet):
         token.save()
         serializer = TokenSerializer(token)
         return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        """ Update token (note that key and user cannot be changed) """
+        return self.partial_update(request, pk)
+
+    def partial_update(self, request, pk=None):
+        """ Partial updates field by field """
+        token = self._token(request, pk)
+        token.name = request.data.get('name')
+        token.save()        
+        serializer = TokenSerializer(token)
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        token = self._token(request, pk)
+        token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
