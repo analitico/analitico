7147afaa41bae65bb2e4e23a235c6687c082d2dc
Gionata Mettifogo <gmettifogo@gmail.com>
2018-12-17 16:58:48 +0100

    False in git

--- source/website/settings.py
+++ source/website/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '111&xe5+tyf29&&%t!jk9-v)!v07gc%0ha4*4#8e+rfd@7i80#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
