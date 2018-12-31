3ad7caa04e2387318d3585f8d47b086fdc65d882
Gionata <gmettifogo@gmail.com>
2018-12-31 13:36:43 +0100

    Paginator

--- source/api/pagination.py
+++ source/api/pagination.py
@@ -1,6 +1,23 @@
 
 from rest_framework import pagination
+from rest_framework.response import Response
+from collections import OrderedDict, namedtuple
 
-class AnaliticoPagination(pagination.PageNumberPagination):
-    pass
+import rest_framework_json_api.pagination
+import rest_framework.renderers
+import rest_framework_json_api.renderers
 
+#class AnaliticoPageNumberPagination(pagination.PageNumberPagination):
+class AnaliticoPageNumberPagination(rest_framework_json_api.pagination.JsonApiPageNumberPagination):
+    
+    page_query_param = 'page'
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+#    def get_paginated_response(self, data):
+#        return Response(OrderedDict([
+#            ('count', self.page.paginator.count),
+#            ('next', self.get_next_link()),
+#            ('previous', self.get_previous_link()),
+#            ('data', data)
+#        ]))
