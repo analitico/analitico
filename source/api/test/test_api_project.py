5b348707616b020623c880ce8a25a711ea871b5d
Gionata <gmettifogo@gmail.com>
2018-12-31 13:40:36 +0100

    Adding tests with and without auth tokens

--- source/api/test/test_api_project.py
+++ source/api/test/test_api_project.py
@@ -1,7 +1,11 @@
 
+import rest_framework.test
+import api.test
+
+from django.urls import reverse
 from django.test import TestCase
-from rest_framework.test import APITestCase
-from api.models import Project
+
+from api.models import Project, Token, Call, User
 
 # pylint: disable=no-member
 
@@ -14,28 +18,45 @@ PRJ_SETTINGS = {
     }
 }
 
-class ProjectsApiTests(APITestCase):
+
+class ProjectsApiTests(api.test.APITestCase):
 
     def setUp(self):
-        Project.objects.create(id='s24-order-time', settings=PRJ_SETTINGS)
+        super().setUp()
+        self.project1 = Project.objects.create(id='test-project1', settings=PRJ_SETTINGS, owner=self.user1)
 
-    def test_project_model_with_existing_project(self):
-        prj1 = Project.objects.get(pk='s24-order-time')
-        self.assertIsNotNone(prj1)
-        self.assertEqual(prj1.settings['data']['parameters']['learning_rate'], 0.50)
-        self.assertEqual(prj1.settings['data']['parameters']['iterations'], 30)
 
     def test_project_api_with_existing_project(self):
-        response = self.client.get('/api/v1/project/s24-order-time/', format='json')
+        self.auth_token(self.token1)
+        response = self.client.get('/api/v1/projects/test-project1/', format='json')
         self.assertIsNotNone(response.data)
-        settings = response.data['data']['settings']['data']
+        settings = response.data['settings']
         self.assertIsNotNone(settings)
-        self.assertEqual(settings['parameters']['learning_rate'], 0.50)
-        self.assertEqual(settings['parameters']['iterations'], 30)
+        self.assertEqual(settings['data']['parameters']['learning_rate'], PRJ_SETTINGS['data']['parameters']['learning_rate'])
+        self.assertEqual(settings['data']['parameters']['iterations'], PRJ_SETTINGS['data']['parameters']['iterations'])
+
+
+    def test_project_api_with_existing_project_no_token(self):
+        self.auth_token()
+        response = self.client.get('/api/v1/projects/s24-order-time/', format='json')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_text, 'Unauthorized')
+        self.assertEqual(response.data['error']['code'], 'not_authenticated')
+
 
     def test_project_api_with_missing_project(self):
-        response = self.client.get('/api/v1/project/fake-id/', format='json')
+        self.auth_token(self.token1)
+        response = self.client.get('/api/v1/projects/fake-id/', format='json')
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.reason_phrase, 'Not Found')
+        self.assertEqual(response.data['error']['code'], 'Not Found')
         self.assertEqual(response.data['error']['status'], '404')
 
+
+    def test_project_api_with_missing_project_no_token(self):
+        self.auth_token()
+        response = self.client.get('/api/v1/projects/fake-id/', format='json')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.reason_phrase, 'Unauthorized')
+        self.assertEqual(response.data['error']['code'], 'not_authenticated')
+        self.assertEqual(response.data['error']['status'], '401')
