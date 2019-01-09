ac7a6270c3534f9a6f17b634051b012592eec679
Gionata <gmettifogo@gmail.com>
2019-01-09 21:58:40 +0100

    Create users with different rights

--- source/api/test/utils.py
+++ source/api/test/utils.py
@@ -12,10 +12,13 @@ class APITestCase(rest_framework.test.APITestCase):
     """ Base class for testing analitico APIs """
 
     def setup_basics(self):
-        self.user1 = User.objects.create_user(email='user1@analitico.ai')
-        self.user2 = User.objects.create_user(email='user2@analitico.ai')
+        self.user1 = User.objects.create_user(email='user1@analitico.ai', is_superuser=True) # 1st user is admin
+        self.user2 = User.objects.create_user(email='user2@analitico.ai') # 2nd is a regular user
+        self.user3 = User.objects.create_user(email='user3@analitico.ai') # 3rd is a regular user
+
         self.token1 = Token.objects.create(pk='tok_user1', user=self.user1)
         self.token2 = Token.objects.create(pk='tok_user2', user=self.user2)
+        self.token3 = Token.objects.create(pk='tok_user3', user=self.user3)
 
 
     def setUp(self):
