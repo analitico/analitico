8d4b7e86dfbdf49866af477347a756bcaf16cbc3
Gionata <gmettifogo@gmail.com>
2019-01-09 21:59:24 +0100

    Added tests for DELETE method

--- source/api/test/test_api_items.py
+++ source/api/test/test_api_items.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from analitico.utilities import read_json
+from analitico.utilities import read_json, get_dict_dot
 
 import api.models
 import api.test
@@ -40,11 +40,26 @@ class ItemsTests(api.test.APITestCase):
         return response.data
 
 
+    def delete_item(self, item_type, item_id, token=None, status_code=status.HTTP_200_OK):
+        url = reverse('api:' + item_type + '-detail', args=(item_id,))
+        self.auth_token(token)
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status_code)
+        return response.data
+
+
     def upload_items(self, endpoint, prefix):
         for path in os.listdir(ASSETS_PATH):
             if path.startswith(prefix):
                 item = self.read_json_asset(path)
-                self.auth_token(self.token1)
+
+                token = self.token1
+                if (get_dict_dot(item, 'attributes.user') == 'user2@analitico.ai'):
+                    token = self.token2
+                if (get_dict_dot(item, 'attributes.user') == 'user3@analitico.ai'):
+                    token = self.token3
+
+                self.auth_token(token)
                 response = self.client.post(endpoint, { 'data': item }, format='json')
                 self.assertIsNotNone(response.data)
 
@@ -91,6 +106,15 @@ class ItemsTests(api.test.APITestCase):
     def test_workspace_get(self):
         item = self.get_item('workspace', 'ws_001', self.token1)
         self.assertEqual(item['id'], 'ws_001')
+        self.assertEqual(item['attributes']['user'], 'user1@analitico.ai')
+        self.assertEqual(item['attributes']['title'], 'This is the title')
+        self.assertEqual(item['attributes']['description'], 'This is the description')
+
+
+    def test_workspace_get_user2(self):
+        item = self.get_item('workspace', 'ws_002', self.token1)
+        self.assertEqual(item['id'], 'ws_002')
+        self.assertEqual(item['attributes']['user'], 'user2@analitico.ai')
         self.assertEqual(item['attributes']['title'], 'This is the title')
         self.assertEqual(item['attributes']['description'], 'This is the description')
 
@@ -236,6 +260,34 @@ class ItemsTests(api.test.APITestCase):
         patch['data']['attributes']['made_up_attribute'] = None # them removing it
         patch_item = self.patch_item('workspace', 'ws_001', patch, self.token1)        
         self.assertIsNone(patch_item['attributes']['made_up_attribute'])
+
+
+    def test_workspace_delete(self):
+        item = self.delete_item('workspace', 'ws_001', self.token1, status.HTTP_204_NO_CONTENT)
+        self.assertIsNone(item)
+
+        # no try and get deleted item, should return a 404
+        item = self.delete_item('workspace', 'ws_001', self.token1, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(item['error']['code'], 'Not Found')
+        self.assertIsNotNone(item['error']['detail'])
+        self.assertEqual(item['error']['status'], '404') # a string, not a number
+
+
+    def test_workspace_delete_without_authorization(self):
+        # user2 is not the owner of this workspace so, altough it does exist,
+        # the server should pretend it's not there (which it isn't for this user)
+        # and return an item not found code of HTTP 404
+        item = self.delete_item('workspace', 'ws_001', self.token2, status_code=status.HTTP_404_NOT_FOUND)
+        self.assertEqual(item['error']['code'], 'Not Found')
+        self.assertIsNotNone(item['error']['detail'])
+        self.assertEqual(item['error']['status'], '404') # a string, not a number
+
+
+    def test_workspace_delete_without_authorization_as_admin(self):
+        # ws_002 is owned by user2@analitico.ai but user1 is an admin so he should be able to delete it
+        item = self.delete_item('workspace', 'ws_002', token=self.token1, status_code=status.HTTP_204_NO_CONTENT)
+        self.assertIsNone(item)
+
 
 
 
