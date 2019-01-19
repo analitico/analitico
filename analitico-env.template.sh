1092624f25f689e1e12f37e797428e46d1f7c926
Gionata <gmettifogo@gmail.com>
2019-01-19 10:01:10 +0100

    Environment variables template

---
+++ analitico-env.template.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Sets the environment variables required by analitico server
+# This file is not committed in the main repository but is installed
+# and run in development and production with different settings
+#
+# Apply environment to shell using:
+# source analitico-env.sh
+#
+# Apply environment variables automatically by adding them to:
+# ~/.bash_login
+
+echo "analitico.ai: setting environment variables"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+export ANALITICO_DEBUG="False"
+
+# Django secret key
+export ANALITICO_SECRET_KEY="xxx"
+
+# MySQL configuration
+export ANALITICO_MYSQL_HOST="xxx"
+export ANALITICO_MYSQL_NAME="xxx"
+export ANALITICO_MYSQL_USER="xxx"
+export ANALITICO_MYSQL_PASSWORD="xxx"
+
+# Google Cloud Storage private keys (note: using $ to handle escape chars in secret)
+export ANALITICO_GCS_KEY="xxx@xxx.com"
+export ANALITICO_GCS_SECRET=$"-----BEGIN PRIVATE KEY-----\nxxx\n-----END PRIVATE KEY-----\n"
+export ANALITICO_GCS_PROJECT="xxx"
