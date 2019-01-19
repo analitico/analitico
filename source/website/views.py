defae4e0c4628b2f4e74bba214149d2969bc6972
Gionata <gmettifogo@gmail.com>
2019-01-19 10:00:48 +0100

    Test views

---
+++ source/website/views.py
@@ -0,0 +1,19 @@
+
+from django.shortcuts import render
+
+def home(request):
+	return render(request, 'home.html')
+
+
+def pippo(request):
+	return render(request, 'pippo.html')
+
+
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+
+@login_required
+def lab(request):
+    return render(request, 'lab.html')
