4a55d73c58532048047d5495127d830dcc4eb3fd
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-01-24 14:23:39 +0100

    try to run test

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # Build and test execution
-
-echo "Building"
-
-# Load injected env
+echo "Injecting env"
 source analitico-env
+echo "Installing requirements"
 source venv/bin/activate
 pip3 install -r requirements.txt
+
 cd source
+
 echo "Static"
 ./manage.py collectstatic --noinput
-#./manage.py test
+
+echo "Running tests"
+./manage.py test
+
+#sudo ln -s /home/www/analitico/conf/nginx.conf /etc/nginx/
+#nginx
