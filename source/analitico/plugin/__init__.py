1c62f83cb4750d9252592be0d80090408f676acf
Gionata Mettifogo <gmettifogo@gmail.com>
2019-01-29 11:23:49 +0100

    Comments only

--- source/analitico/plugin/__init__.py
+++ source/analitico/plugin/__init__.py
@@ -1,3 +1,4 @@
+
 # plugin base classes
 from .plugin import IPlugin  # NOQA: F401
 from .plugin import IDataframeSourcePlugin  # NOQA: F401
@@ -6,25 +7,26 @@ from .plugin import IGroupPlugin  # NOQA: F401
 from .plugin import IPluginManager  # NOQA: F401
 from .plugin import PluginError  # NOQA: F401
 
-# plugins to generate a dataframe from a source
+# plugins to generate dataframes from sources
 from .sources import CsvDataframeSourcePlugin
 
-# plugins that tranform a dataframe
+# plugins to tranform dataframes
 from .transforms import CodeDataframePlugin
 
-# plugin grouping
+# plugin workflows
 from .groups import PipelinePlugin
 from .groups import GraphPlugin
 
 # plugin factory
 from .manager import PluginManager, manager  # NOQA: F401
 
-# Constants with plugin names
+# plugin names
 CSV_DATAFRAME_SOURCE_PLUGIN = CsvDataframeSourcePlugin.Meta.name
 CODE_DATAFRAME_PLUGIN = CodeDataframePlugin.Meta.name
 PIPELINE_PLUGIN = PipelinePlugin.Meta.name
 GRAPH_PLUGIN = GraphPlugin.Meta.name
 
+# analitico type for plugins
 PLUGIN_TYPE = "analitico/plugin"
 
 # NOQA: F401 prospector complains that these imports
