eb3e18f14cc5cc0d4426c1e9eb81a48d1f861bb7
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-01-31 14:33:01 +0100

    removed "staging" from ssl env variable names because now ssl certificate is wildcard remove all unused images from server when deploying

--- build.sh
+++ build.sh
@@ -32,8 +32,8 @@ sudo ln -s /home/www/analitico/conf/nginx.conf /etc/nginx/nginx.conf
 
 # TODO: copy SSL certificates
 mkdir -p /home/www/ssl
-echo "$ANALITICO_STAGING_SSL_CRT" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/analitico.ai.crt
-echo "$ANALITICO_STAGING_SSL_KEY" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/analitico.ai.key
+echo "$ANALITICO_SSL_CRT" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/analitico.ai.crt
+echo "$ANALITICO_SSL_KEY" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/analitico.ai.key
 chmod 600 /home/www/ssl/analitico.ai.key
 chmod 755 /home/www/ssl/analitico.ai.crt
 echo "Done"
