5c01f78570ed3fb4c4e6694fa0a10eaa5a5b87ba
Gionata Mettifogo <gmettifogo@gmail.com>
2019-02-09 22:38:40 +0100

    Adding concept of payload to Job model

--- source/api/models/job.py
+++ source/api/models/job.py
@@ -264,6 +264,16 @@ class Job(ItemMixin, models.Model):
         """ Returns the target of this job (eg: dataset to be processed, model to be trained, etc) """
         return ModelsFactory.from_id(self.item_id, request)
 
+    ## Properties
+
+    @property
+    def payload(self):
+        """ Payload attached to the job (eg: an inference, job results, etc) """
+        return self.get_attribute("payload", None)
+    @payload.setter
+    def payload(self, payload):
+        self.set_attribute("payload", payload)
+
     ##
     ## Execution
     ##
