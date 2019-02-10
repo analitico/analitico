3b0fb9aa6c60893679fe3c28fa3e635e6705cb61
Gionata Mettifogo <gmettifogo@gmail.com>
2019-02-10 22:54:00 +0100

    Accept ?token=tok_xxx for authentication #15 - BearerAuthentication detects token and authenticates user - need to figure out how to get downstream views to ignore parameter

--- source/api/authentication.py
+++ source/api/authentication.py
@@ -31,24 +31,27 @@ class BearerAuthentication(BaseAuthentication):
         return Token
 
     def authenticate(self, request):
-        auth = get_authorization_header(request).split()
 
-        if not auth or auth[0].lower() != self.keyword.lower().encode():
-            return None
+        # see if token passed as ?token=tok_xxx
+        token = request.query_params.get("token")
 
-        if len(auth) == 1:
-            msg = _("Invalid token header. No credentials provided.")
-            raise exceptions.AuthenticationFailed(msg)
-        elif len(auth) > 2:
-            msg = _("Invalid token header. Token string should not contain spaces.")
-            raise exceptions.AuthenticationFailed(msg)
+        # check for token in authorization headers
+        if not token:
+            auth = get_authorization_header(request).split()
+            if not auth or auth[0].lower() != self.keyword.lower().encode():
+                return None
 
-        try:
-            token = auth[1].decode()
-        except UnicodeError:
-            msg = _("Invalid token header. Token string should not contain invalid characters.")
-            raise exceptions.AuthenticationFailed(msg)
-
+            if len(auth) == 1:
+                msg = _("Invalid token header. No credentials provided.")
+                raise exceptions.AuthenticationFailed(msg)
+            elif len(auth) > 2:
+                msg = _("Invalid token header. Token string should not contain spaces.")
+                raise exceptions.AuthenticationFailed(msg)
+            try:
+                token = auth[1].decode()
+            except UnicodeError:
+                msg = _("Invalid token header. Token string should not contain invalid characters.")
+                raise exceptions.AuthenticationFailed(msg)
         return self.authenticate_credentials(token)
 
     def authenticate_credentials(self, key):
