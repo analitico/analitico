07c8833a43a43265b2be8dd2c8f5dcc67d3d7324
Gionata Mettifogo <gmettifogo@gmail.com>
2019-02-10 10:37:02 +0100

    Renderer fix for corner cases

--- source/api/renderers.py
+++ source/api/renderers.py
@@ -4,11 +4,31 @@ from collections import OrderedDict, namedtuple
 
 import rest_framework.renderers
 import rest_framework_json_api.renderers
-
+from analitico.utilities import get_dict_dot
 
 class JSONRenderer(rest_framework.renderers.JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         # change results to { 'data': results } to be more json:api-ish
-        if data and (not "error" in data) and (not "data" in data):
-            data = {"data": data}
+
+        # this method is used on the server to render response sent back from APIs
+        # it is also used by self.client in unit test to send posts to APIs
+
+        # supported scenarios:
+        # - test is sending a single object (dictionary)
+        # - test is sending multiple objects (array)
+        # - response is sending single object
+
+        if data:
+
+            if isinstance(data, dict):
+                if "error" not in data:
+                    
+                    # in json:api when "data" is present it is the only entry
+                    is_jsonapi_data = "data" in data and len(data) == 1
+                    if not is_jsonapi_data:
+                        data = OrderedDict({"data": data})
+
+            if isinstance(data, list):
+                data = OrderedDict({"data": data})
+        
         return super().render(data, accepted_media_type, renderer_context)
