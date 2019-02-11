487e0a522f111aad063fd8b16c03c3fa3e73012e
Gionata Mettifogo <gmettifogo@gmail.com>
2019-02-11 21:12:17 +0100

    Removing old endpoints

--- source/api/urls.py
+++ source/api/urls.py
@@ -23,14 +23,4 @@ router.register("jobs", api.views.JobViewSet, basename="job")  # sync and async 
 router.register("models", api.views.ModelViewSet, basename="model")  # trained machine learning models
 router.register("endpoints", api.views.EndpointViewSet, basename="endpoint")  # inference delivery endpoint
 
-# deprecated, to be removed soon
-router.register("projects", api.views.ProjectViewSet, basename="project")
-router.register("trainings", api.views.TrainingViewSet, basename="training")
-
-urlpatterns = router.urls + [
-    path("project/<str:project_id>/training", api.views.handle_prj_training),
-    #   path('project/<str:project_id>/inference', api.views.handle_prj_inference),
-    path("project/<str:project_id>/upload/<str:path>", api.views.handle_prj_upload),
-    path("training/<str:training_id>", api.views.handle_trn),
-    path("training/<str:training_id>/activate", api.views.handle_trn_activate),
-]
+urlpatterns = router.urls
