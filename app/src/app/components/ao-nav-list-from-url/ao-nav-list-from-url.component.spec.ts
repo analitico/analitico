4e6cea386462c9accec78246f93f88dc7ba9ec02
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-02-12 18:48:15 +0100

    ao-nav-list-from-url unit test

--- app/src/app/components/ao-nav-list-from-url/ao-nav-list-from-url.component.spec.ts
+++ app/src/app/components/ao-nav-list-from-url/ao-nav-list-from-url.component.spec.ts
@@ -1,25 +1,56 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatListModule, MatIconModule } from '@angular/material';
 import { AoNavListFromUrlComponent } from './ao-nav-list-from-url.component';
+import { RouterModule } from '@angular/router';
+import { AoApiClientService } from 'src/app/services/ao-api-client/ao-api-client.service';
+import { RouterTestingModule } from '@angular/router/testing';
+
+class MockAoApiClientService {
+    get(url: any) {
+        return new Promise((resolve, reject) => {
+            resolve({
+                data: [{
+                    id: 'id1'
+                }]
+            });
+        });
+
+    }
+    delete(url: string) {
+        return {};
+    }
+}
 
 describe('AoNavListFromUrlComponent', () => {
-  let component: AoNavListFromUrlComponent;
-  let fixture: ComponentFixture<AoNavListFromUrlComponent>;
+    let component: AoNavListFromUrlComponent;
+    let fixture: ComponentFixture<AoNavListFromUrlComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AoNavListFromUrlComponent ]
-    })
-    .compileComponents();
-  }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AoNavListFromUrlComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AoNavListFromUrlComponent],
+            imports: [MatListModule, RouterModule, MatIconModule, RouterTestingModule],
+            providers: [{ provide: AoApiClientService, useClass: MockAoApiClientService }]
+        })
+            .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AoNavListFromUrlComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have <a> with item id', fakeAsync(() => {
+        component.url = 'myurl';
+        tick();
+        fixture.detectChanges();
+        const list: HTMLElement = fixture.nativeElement;
+        const a = list.querySelector('a');
+        expect(a.textContent.trim()).toEqual('id1');
+    }));
 });
