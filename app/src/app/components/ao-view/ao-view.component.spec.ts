ca77cdf3825300ba441c9f621263ca55466b6705
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-02-13 11:49:57 +0100

    ao-view unit tests

--- app/src/app/components/ao-view/ao-view.component.spec.ts
+++ app/src/app/components/ao-view/ao-view.component.spec.ts
@@ -1,25 +1,91 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed, tick, fakeAsync, } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { AoApiClientService } from 'src/app/services/ao-api-client/ao-api-client.service';
 import { AoViewComponent } from './ao-view.component';
+import { delay, concatMap } from 'rxjs/operators';
+import { of, from } from 'rxjs';
+
+class MockAoApiClientService {
+    get(url: any) {
+        return new Promise((resolve, reject) => {
+            resolve({
+                data: {
+                    id: 'id1'
+                }
+            });
+        });
+
+    }
+    patch(url: string) {
+        return new Promise((resolve, reject) => {
+            resolve({
+                data: {
+                    id: 'id1',
+                    saved: true
+                }
+            });
+        });
+    }
+}
+
+// mock ActivateRoute returning url and params
+class MockActivatedRoute {
+    url = of([{ path: 'dataset' }]).pipe(delay(100));
+    params = from([{id: '1'}, {id: '2'}]).pipe(
+        concatMap(item => of(item).pipe(delay(1000)))
+    );
+}
 
 describe('AoViewComponent', () => {
-  let component: AoViewComponent;
-  let fixture: ComponentFixture<AoViewComponent>;
+    let component: AoViewComponent;
+    let fixture: ComponentFixture<AoViewComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AoViewComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AoViewComponent],
+            providers: [
+                { provide: AoApiClientService, useClass: MockAoApiClientService },
+                { provide: ActivatedRoute, useClass: MockActivatedRoute }]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AoViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AoViewComponent);
+        component = fixture.componentInstance;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        fixture.detectChanges();
+        expect(component).toBeTruthy();
+    });
+
+    it('should get url path and object id', fakeAsync(() => {
+        const spy = spyOn(component, 'onLoad');
+        fixture.detectChanges();
+        tick(1850);
+        fixture.detectChanges();
+        expect(component.baseUrl).toEqual('/dataset');
+        expect(component.objectId).toEqual('1');
+        expect(spy).toHaveBeenCalled();
+        tick(1850);
+        fixture.detectChanges();
+        expect(component.objectId).toEqual('2');
+        expect(spy).toHaveBeenCalled();
+    }));
+
+    it('should save item', fakeAsync(() => {
+        const spy = spyOn(component, 'onSaved');
+        fixture.detectChanges();
+        tick(3000);
+        fixture.detectChanges();
+        expect(component.objectId).toEqual('2');
+        // save
+        component.saveItem();
+        tick(1000);
+        fixture.detectChanges();
+        expect(component.objectId).toEqual('2');
+        expect(component.item.saved).toBeTruthy();
+        expect(spy).toHaveBeenCalled();
+    }));
 });
