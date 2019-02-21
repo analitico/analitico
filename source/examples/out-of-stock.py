18d9845de0ed746b01d605279f0976ba02af5da8
Gionata Mettifogo <gmettifogo@gmail.com>
2019-02-21 12:21:14 +0100

    Working on notebooks

---
+++ source/examples/out-of-stock.py
@@ -0,0 +1,26 @@
+import os
+
+print(os.getcwd())
+print(os.listdir())
+print(os.listdir("analitico"))
+
+import requests
+import sys
+
+# sys.path.append('/home/gionata/github/analitico/source/')
+
+import analitico
+import pandas as pd
+
+TOKEN = "tok_tester1_A7HMc7FA"
+ENDPOINT = "https://staging.analitico.ai/api/"
+
+api = analitico.authorize(token=TOKEN, endpoint=ENDPOINT)
+
+# json = requests.post(ENDPOINT + "datasets/ds_s24_courier/jobs/process").json()
+
+ds = api.get_dataset("ds_s24_order_detail")
+df = ds.get_dataframe()
+
+
+print("done")
