9d8abe23829a5d2c3a5a15261466d0f680799618
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-02-22 19:01:22 +0100

    csv dataframe plugin ui prototype

--- app/src/app/plugins/ao-csv-dataframe-source-plugin/ao-csv-dataframe-source-plugin.component.ts
+++ app/src/app/plugins/ao-csv-dataframe-source-plugin/ao-csv-dataframe-source-plugin.component.ts
@@ -4,6 +4,9 @@
  */
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AoPluginComponent } from 'src/app/plugins/ao-plugin-component';
+import { AoApiClientService } from 'src/app/services/ao-api-client/ao-api-client.service';
+import { query } from '@angular/core/src/render3';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
     selector: 'app-ao-csv-dataframe-source-plugin',
@@ -11,11 +14,48 @@ import { AoPluginComponent } from 'src/app/plugins/ao-plugin-component';
     styleUrls: ['./ao-csv-dataframe-source-plugin.component.css']
 })
 export class AoCsvDataframeSourcePluginComponent extends AoPluginComponent {
-    uploadUrl = './assets';
+    rows: any;
+    displayedColumns: any;
+    tableDS: any;
 
-    constructor() {
+    constructor(protected apiClient: AoApiClientService) {
         super();
     }
+    setData(data: any) {
+        super.setData(data);
+        this.loadSource();
+    }
 
+    loadSource() {
+        // load first 100 rows
+        const url = this.data.source.url.substring(this.data.source.url.indexOf('/datasets')) + '?format=json&page=0&page_size=100';
+        this.apiClient.get(url)
+            .then((response: any) => {
+                this.rows = response.data;
+                this.buildTable();
+            });
+    }
 
+    buildTable() {
+        this.displayedColumns = ['columnName'];
+        const tableRows = [];
+        const tableRowsDic = {};
+        this.data.source.schema.columns.forEach(col => {
+            const dic = {columnName: col.name};
+            tableRows.push(dic);
+            // keeps reference
+            tableRowsDic[col.name] = dic;
+        });
+        this.rows.forEach((row, index) => {
+            // add column
+            this.displayedColumns.push('' + index);
+            this.data.source.schema.columns.forEach(col => {
+                // get row for this column
+                const rowForColumn = tableRowsDic[col.name];
+                // copy column value in row index position
+                rowForColumn[index] = row[col.name];
+            });
+        });
+        this.tableDS = new MatTableDataSource(tableRows);
+    }
 }
