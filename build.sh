3add855c3b05b806a7fc742a9fc2123564b45acb
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-02-23 07:12:34 +0100

    setup ssl certificates and nginx conf before running tests

--- build.sh
+++ build.sh
@@ -7,6 +7,23 @@ source analitico-env
 export LANG=C.UTF-8
 export LC_CTYPE=C.UTF-8
 
+echo "Link nginx conf"
+sudo ln -s /home/www/analitico/conf/nginx.conf /etc/nginx/nginx.conf
+
+# copy SSL certificates
+mkdir -p /home/www/ssl
+echo "$ANALITICO_SSL_CRT" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/analitico.ai.crt
+echo "$ANALITICO_SSL_KEY" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/analitico.ai.key
+chmod 600 /home/www/ssl/analitico.ai.key
+chmod 755 /home/www/ssl/analitico.ai.crt
+
+# copy CloudSQL certificates
+mkdir -p /home/www/ssl/cloudsql
+echo "$ANALITICO_MYSQL_SSL_CA" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/cloudsql/client-ca.pem
+echo "$ANALITICO_MYSQL_SSL_CERT" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/cloudsql/client-cert.pem
+echo "$ANALITICO_MYSQL_SSL_KEY" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/cloudsql/client-key.pem
+chmod 644 /home/www/ssl/cloudsql/*
+
 echo "Installing requirements"
 source venv/bin/activate
 pip3 install -r requirements.txt
@@ -28,23 +45,6 @@ ng test
 
 echo "Build Angular app"
 ng build --prod --outputHashing=all
-
-echo "Link nginx conf"
-sudo ln -s /home/www/analitico/conf/nginx.conf /etc/nginx/nginx.conf
-
-# copy SSL certificates
-mkdir -p /home/www/ssl
-echo "$ANALITICO_SSL_CRT" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/analitico.ai.crt
-echo "$ANALITICO_SSL_KEY" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/analitico.ai.key
-chmod 600 /home/www/ssl/analitico.ai.key
-chmod 755 /home/www/ssl/analitico.ai.crt
-
-# copy CloudSQL certificates
-mkdir -p /home/www/ssl/cloudsql
-echo "$ANALITICO_MYSQL_SSL_CA" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/cloudsql/client-ca.pem
-echo "$ANALITICO_MYSQL_SSL_CERT" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/cloudsql/client-cert.pem
-echo "$ANALITICO_MYSQL_SSL_KEY" | base64 -d -w0 | tr -d '\r' > /home/www/ssl/cloudsql/client-key.pem
-chmod 644 /home/www/ssl/cloudsql/*
 
 # make tmp and subfolders public
 chmod -R 777 /tmp
