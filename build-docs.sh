9f7659de4f00f4fe57839a7cbee8e973b1365a12
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-02-26 10:06:50 +0100

    using venv for documentation building

--- build-docs.sh
+++ build-docs.sh
@@ -2,10 +2,16 @@
 # Build static documentation
 # exit if error
 set -e
-cd /home/www/analitico/documentation
 # importing env
 source /home/www/analitico/analitico-env
 export LANG=C.UTF-8
 export LC_CTYPE=C.UTF-8
+
+cd /home/www/analitico/
+echo "Installing requirements"
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+cd /home/www/analitico/documentation
 # build docs
 mkdocs build
