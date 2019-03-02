bd5796bce1e8c0d496db01b5ed7f32f65721578a
Gionata Mettifogo <gmettifogo@gmail.com>
2019-03-02 17:06:21 +0100

    Moving scrap to analitico-scrapbook

--- scrap/s24/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class S24Config(AppConfig):
-    name = "s24"
--- scrap/s24/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .ordertime import OrderTimeModel
-from .outofstock import OutOfStockModel
-from .ordersorting import OrderSortingModel
