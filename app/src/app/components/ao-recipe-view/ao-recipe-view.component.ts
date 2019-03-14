0e763e97e242b4634b7b3005ac26b999d54c7999
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-03-14 07:36:24 +0100

    recipe view dev in progress

--- app/src/app/components/ao-recipe-view/ao-recipe-view.component.ts
+++ app/src/app/components/ao-recipe-view/ao-recipe-view.component.ts
@@ -11,6 +11,7 @@ import { AoPipelineViewComponent } from '../ao-pipeline-view/ao-pipeline-view.co
 import { AoItemService } from 'src/app/services/ao-item/ao-item.service';
 import { MatTableDataSource } from '@angular/material';
 import { AoGlobalStateStore } from 'src/app/services/ao-global-state-store/ao-global-state-store.service';
+import { AoMessageBoxService } from 'src/app/services/ao-message-box/ao-message-box';
 
 @Component({
     templateUrl: './ao-recipe-view.component.html',
@@ -20,7 +21,15 @@ export class AoRecipeViewComponent extends AoPipelineViewComponent implements On
 
     isProcessing = false;
     models: any;
+    endpoints: any;
     tableModels: any;
+    activeTabIndex: any;
+    tabRouteSubscription: any;
+    datasets: any;
+    selectedDataset: any;
+    currentView: string;
+    hasPipeline = false; // if the recipe already has a pipeline
+    selectedModel: any;
 
     constructor(route: ActivatedRoute, apiClient: AoApiClientService,
         protected componentFactoryResolver: ComponentFactoryResolver,
@@ -28,7 +37,8 @@ export class AoRecipeViewComponent extends AoPipelineViewComponent implements On
         protected snackBar: MatSnackBar,
         protected jobService: AoJobService,
         protected itemService: AoItemService,
-        protected router: Router) {
+        protected router: Router,
+        protected messageBox: AoMessageBoxService) {
         super(route, apiClient, componentFactoryResolver, pluginsService, snackBar, itemService);
     }
 
@@ -37,29 +47,110 @@ export class AoRecipeViewComponent extends AoPipelineViewComponent implements On
 
     }
 
-    onLoad() {
-        super.onLoad();
-        this.loadModels();
+    ngOnDestroy() {
+        super.ngOnDestroy();
+        if (this.tabRouteSubscription) {
+            this.tabRouteSubscription.unsubscribe();
+        }
     }
 
-    loadModels() {
-        this.itemService.getModels()
-            .then((models: any) => {
-                this.models = [];
-                models.forEach(model => {
-                    if (model.attributes.recipe_id === this.item.id) {
-                        this.models.push(model);
-                    }
-                });
-                // sort updated_at desc
-                this.models.sort(function (a, b) {
-                    return a.attributes.updated_at > b.attributes.updated_at ? -1 : 1;
-                });
-                // assign data source for the table
-                this.tableModels = new MatTableDataSource(this.models);
+    /**
+     * override method
+     */
+    loadItem() {
+        return this.itemService.getRecipeById(this.objectId)
+            .then((recipe: any) => {
+                this.item = recipe;
+                this.hasPipeline = this.item.attributes && this.item.attributes.plugin && this.item.attributes.plugin.plugins &&
+                    this.item.attributes.plugin.plugins.length > 1;
+                this.title = (this.item.attributes && this.item.attributes.title) || this.item.id;
+                this.description = this.item.description;
+                this.models = recipe._aoprivate.models;
+                this.endpoints = recipe._aoprivate.endpoints;
+                if (this.endpoints && this.endpoints.length === 1) {
+                    this.selectedModel = this.endpoints[0]._aoprivate.model;
+                }
+                this.onLoad();
+            })
+            .catch((response) => {
+                if (response.status === 404) {
+                    window.location.href = '/app';
+                }
             });
     }
 
+    onLoad() {
+        super.onLoad();
+        if (this.tabRouteSubscription) {
+            this.tabRouteSubscription.unsubscribe();
+        }
+        this.tabRouteSubscription = this.route.queryParams.subscribe(this.checkRouteForTab.bind(this));
+
+        if (this.models) {
+            // assign data source for the table
+            this.tableModels = new MatTableDataSource(this.models);
+        }
+
+        this.loadDatasets();
+    }
+
+
+    // update selected tab according to view parameters
+    // redirect to live view if there is an endpoint
+    // redirect to recipe view if no endpoints
+    checkRouteForTab(params: any) {
+        this.currentView = params.view;
+        if (!this.currentView && this.endpoints && this.endpoints.length > 0) {
+            return this.router.navigate([], { queryParams: { view: 'live' }, relativeTo: this.route, replaceUrl: true });
+        }
+
+        if (this.currentView === 'live' && (!this.endpoints || this.endpoints.length === 0)) {
+            return this.router.navigate([], { queryParams: { view: 'recipe' }, relativeTo: this.route, replaceUrl: true });
+        }
+        if (this.currentView === 'live') {
+            this.activeTabIndex = 2;
+        } else if (this.currentView === 'models') {
+            this.activeTabIndex = 1;
+        } else {
+            this.activeTabIndex = 0;
+        }
+    }
+
+    // change view parameters when tab is selected
+    selectedTabChanged(tabIndex) {
+        if (tabIndex === 0) {
+            this.router.navigate([], { queryParams: { view: 'recipe' }, relativeTo: this.route });
+        } else if (tabIndex === 1) {
+            this.router.navigate([], { queryParams: { view: 'models' }, relativeTo: this.route });
+        } else if (tabIndex === 2) {
+            this.router.navigate([], { queryParams: { view: 'live' }, relativeTo: this.route });
+
+        }
+    }
+    // returns the dataset associated with the recipe (if any)
+    getRecipeDatasetId() {
+        try {
+            return this.item.attributes['plugin'].plugins[0].source.dataset_id;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    // load datasets that can be associated with this recipe
+    loadDatasets() {
+        this.itemService.getDatasets()
+            .then((datasets) => {
+                // filter datasets according to recipe workspace
+                this.datasets = this.itemService.filterItemsByDictionary(datasets,
+                    { 'attributes.workspace_id': this.item.attributes.workspace_id });
+                // select dataset
+                const datasetId = this.getRecipeDatasetId();
+                this.selectedDataset = this.itemService.getItemById(datasets, datasetId);
+
+            });
+    }
+
+    // train the recipe
     train() {
         if (this.isProcessing) {
             return;
@@ -127,13 +218,92 @@ export class AoRecipeViewComponent extends AoPipelineViewComponent implements On
         });
     }
 
+    // create a new end point for a given model
     createEndpointForModel(model) {
         this.itemService.createEndpointForModel(model)
             .then((endpoint) => {
                 // open the endpoint page
-                this.router.navigate(['/endpoints/' + endpoint.id]);
+                // this.router.navigate(['/endpoints/' + endpoint.id]);
+                this.activeTabIndex = 2;
             });
 
+    }
+
+    // ask to change the model associated with this endpoint
+    requestToSetNewModel(model) {
+        const subscription =
+            this.messageBox.show('Do you want to change the model associated with this endpoint?',
+                'Changing endpoint model', 'WARNING: this could affect your production data', 2)
+                .subscribe((response) => {
+                    subscription.unsubscribe();
+                    if (response.result === 'yes') {
+                        this.setModel(model);
+                    }
+                });
+    }
+
+    // sets the model_id of the first endpoint and reloads item
+    setModel(model) {
+        if (this.endpoints.length === 0) {
+            this.createEndpointForModel(model);
+        } else if (this.endpoints.length === 1) {
+            // change the first endpoint
+            return this.itemService.changeEndpointModel(this.endpoints[0], model.id)
+                .then(() => {
+                    this.loadItem();
+                });
+        }
+    }
+
+    // change the dataset in the recipe
+    changedDataset() {
+        this.setRecipeDataset(this.selectedDataset.id);
+    }
+
+    setRecipeDataset(datasetId) {
+        if (!this.item.attributes.plugin) {
+            this.item.attributes['plugin'] = {
+                'type': 'analitico/plugin',
+                'name': 'analitico.plugin.RecipePipelinePlugin',
+                'plugins': []
+            };
+        }
+        // add datasource with the selected dataset
+        this.item.attributes['plugin'].plugins[0] = {
+            'type': 'analitico/plugin',
+            'name': 'analitico.plugin.DatasetSourcePlugin',
+            'source': {
+                'dataset_id': datasetId
+            }
+        };
+
+        return this.saveItem();
+    }
+
+    // create a new dataset and return asset upload url
+    getNewDatasetUploadUrl = (file) => {
+        // create dataset
+        return this.apiClient.post('/datasets', { workspace_id: this.item.attributes.workspace_id })
+            .then((response: any) => {
+                return { file: file, url: '/api/datasets/' + response.data.id + '/assets' };
+            });
+    }
+
+    // open dataset view for the uploaded dataset
+    afterNewDatasetUploaded = (fileItem) => {
+        if (fileItem.uploadUrl) {
+            const datasetId = (fileItem.uploadUrl.substring(
+                fileItem.uploadUrl.indexOf('/datasets/') + 10).replace('/assets', ''));
+
+            this.setRecipeDataset(datasetId);
+            // process dataset
+            const that = this;
+            return this.itemService.processDataset(datasetId)
+                .catch(() => {
+
+                });
+
+        }
     }
 
 }
