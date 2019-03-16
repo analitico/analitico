ff1d8ae1e807c6033b2d2cea1214c331ea4a110e
Gionata Mettifogo <gmettifogo@gmail.com>
2019-03-16 17:35:09 +0100

    Add support for etags in get responses

--- source/website/settings.py
+++ source/website/settings.py
@@ -212,6 +212,7 @@ try:
         "corsheaders.middleware.CorsMiddleware",
         "django.middleware.security.SecurityMiddleware",
         "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.http.ConditionalGetMiddleware",
         "django.middleware.common.CommonMiddleware",
         "django.middleware.csrf.CsrfViewMiddleware",
         "django.contrib.auth.middleware.AuthenticationMiddleware",
