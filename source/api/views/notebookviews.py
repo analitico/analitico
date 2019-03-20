df96c3e5d0827480478e79d3eb9bb86f8320c7fc
Gionata Mettifogo <gmettifogo@gmail.com>
2019-03-20 15:31:30 +0100

    Generic mixin for /api/type/id/notebook

--- source/api/views/notebookviews.py
+++ source/api/views/notebookviews.py
@@ -3,10 +3,12 @@ import json
 
 from django.http import HttpResponse
 
-from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework import serializers, status
 from rest_framework.exceptions import NotFound
 from rest_framework.decorators import action, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer, StaticHTMLRenderer
 
 import api.models
 import api.utilities
@@ -38,38 +40,65 @@ class NotebookSerializer(AttributeSerializerMixin, serializers.ModelSerializer):
 
 
 ##
+## NotebookViewSetMixin - endpoints for creating jobs attached to an item, eg: train a model
+##
+
+
+class NotebookViewSetMixin:
+    """ Provides APIs to operate on Jupyter notebooks contained in the model. """
+
+    renderers = (JSONRenderer, StaticHTMLRenderer)
+
+    def get_notebook_response(self, name, format, template):
+        item = self.get_object()
+        notebook = item.get_notebook(name)
+        if format == "html":
+            if not notebook:
+                raise NotFound("Notebook model " + item.id + " does not contain the requested Jupyter notebook yet.")
+            content, _ = nb_convert_to_html(notebook, template)
+            return Response(content, content_type="text/html")
+        return Response(notebook, content_type=NOTEBOOK_MIME_TYPE)
+
+    @permission_classes((IsAuthenticated,))
+    @action(methods=["get", "put", "post", "patch"], detail=True, url_name="detail-notebook", url_path="notebook")
+    def notebook(self, request, pk):
+        """ Returns the notebook content as is or converted to html with given template """
+        name = api.utilities.get_query_parameter(request, "name", "notebook")
+        format = api.utilities.get_query_parameter(request, "format", "json")
+        template = api.utilities.get_query_parameter(request, "template", "full")
+
+        if request.method in ("POST", "PUT", "PATCH"):
+            # replace existing notebook with given notebook
+            item = self.get_object()
+            old_notebook = item.get_notebook(name)
+            notebook = request.data["data"]
+            assert "data" in request.data  # serializer adds json:api style "data": xxx
+            item.set_notebook(name, notebook)
+            item.save()
+            # https://restfulapi.net/http-methods/
+            return Response(status=status.HTTP_200_OK if old_notebook else status.HTTP_201_CREATED)
+
+        return self.get_notebook_response(name, format, template)
+
+
+##
 ## NotebookViewSet - list, detail, edit, post and run notebooks
 ##
 
 
 class NotebookViewSet(
-    ItemViewSetMixin, AssetViewSetMixin, JobViewSetMixin, LogViewSetMixin, rest_framework.viewsets.ModelViewSet
+    ItemViewSetMixin,
+    AssetViewSetMixin,
+    JobViewSetMixin,
+    LogViewSetMixin,
+    NotebookViewSetMixin,
+    rest_framework.viewsets.ModelViewSet,
 ):
     """ An editable and runnable Jupyter notebook and its artifacts """
 
     item_class = api.models.Notebook
     serializer_class = NotebookSerializer
     job_actions = (ACTION_PROCESS,)
-
-    @permission_classes((IsAuthenticated,))
-    @action(methods=["get"], detail=True, url_name="detail-notebook", url_path="notebook")
-    def notebook(self, request, pk):
-        """ Returns the notebook content """
-        item = self.get_object()
-        content = json.dumps(item.notebook, indent=4) if item.notebook else ""
-        response = HttpResponse(content=content, content_type=NOTEBOOK_MIME_TYPE)
-        return response
-
-    @permission_classes((IsAuthenticated,))
-    @action(methods=["get"], detail=True, url_name="detail-html", url_path="html")
-    def html(self, request, pk):
-        """ Returns the notebook content converted to html, accepts ?template parameter for 'basic' or 'full' templates """
-        item = self.get_object()
-        if not item.notebook:
-            raise NotFound("Notebook model " + self.id + " does not contain an actual Jupyter notebook yet.")
-        template = api.utilities.get_query_parameter(request, "template", "full")
-        content, _ = nb_convert_to_html(item.notebook, template)
-        return HttpResponse(content=content, content_type="text/html")
 
     @permission_classes((IsAuthenticated,))
     @action(methods=["post"], detail=True, url_name="process", url_path="process")
