b0e58c7faccf26fd6b888083c812b42a8257091c
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-04-02 13:12:14 +0200

    test run jupyter docker on gpu machine

---
+++ scripts/docker-jupyter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# run jupyter notebooks in dockers
+export LC_CTYPE=C.UTF-8
+
+mkdir -p /home/www/notebooks
+cd /home/www/analitico
+source venv/bin/activate
+
+jupyter notebook --port=$1 --ip=0.0.0.0 --no-browser \
+--keyfile='/home/www/analitico-ci/ssl/analitico.ai.key' \
+--certfile='/home/www/analitico-ci/ssl/analitico.ai.crt' \
+--notebook-dir='/home/www/notebooks'  \
+--NotebookApp.allow_origin='*' \
+--NotebookApp.token='$2'
+--NotebookApp.disable_check_xsrf=True &
+
+wait
