af31087899a33282f8bfa1dc0bb86e1e6b89604b
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-04-03 15:11:05 +0200

    prepare docker image for jupyter in CI

--- scripts/docker-jupyter.sh
+++ scripts/docker-jupyter.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # run jupyter notebooks in dockers
 export LC_CTYPE=C.UTF-8
-
-mkdir -p /home/www/notebooks
-cd /home/www/analitico
+cd ~
 source venv/bin/activate
 
 jupyter notebook --port=$1 --ip=0.0.0.0 --no-browser \
---keyfile='/home/www/analitico-ci/ssl/analitico.ai.key' \
---certfile='/home/www/analitico-ci/ssl/analitico.ai.crt' \
---notebook-dir='/home/www/notebooks'  \
+--keyfile='~/analitico.ai.key' \
+--certfile='~/analitico.ai.crt' \
+--notebook-dir='~/notebooks'  \
 --NotebookApp.allow_origin='*' \
 --NotebookApp.token="$2"
 --NotebookApp.disable_check_xsrf=True &
