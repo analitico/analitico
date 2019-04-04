71d7bb5e2e4a21e2b95168f571dc07b9f3d12018
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-04-04 10:09:26 +0200

    test using self signed ssl certificates for jupyter notebook to prevent security breach

--- scripts/docker-jupyter.sh
+++ scripts/docker-jupyter.sh
@@ -3,13 +3,15 @@
 export LC_CTYPE=C.UTF-8
 cd /home/www/analitico/
 source venv/bin/activate
-
+# genereate ssl keys
+openssl req -x509 -nodes -days 7300 -newkey rsa:2048 -keyout key.pem -out cert.pem -subj "/C=US/O=Analitico/CN=analitico.ai"
+# start jupyter notebook
 jupyter notebook --port=$1 --ip=0.0.0.0 --no-browser \
---keyfile='/home/www/analitico/analitico.ai.key' \
---certfile='/home/www/analitico/analitico.ai.crt' \
+--keyfile='/home/www/analitico/key.pem' \
+--certfile='/home/www/analitico/cert.pem' \
 --notebook-dir='/home/www/analitico/notebooks'  \
 --NotebookApp.allow_origin='*' \
 --NotebookApp.token="$2"
 --NotebookApp.disable_check_xsrf=True &
-
+# wait for temination
 wait
