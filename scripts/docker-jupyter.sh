0fddf82a58cbf961fb67380dc566db453bc88192
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-04-04 08:07:04 +0200

    fixed path

--- scripts/docker-jupyter.sh
+++ scripts/docker-jupyter.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # run jupyter notebooks in dockers
 export LC_CTYPE=C.UTF-8
-cd ~
+cd /home/www/analitico/
 source venv/bin/activate
 
 jupyter notebook --port=$1 --ip=0.0.0.0 --no-browser \
---keyfile='~/analitico.ai.key' \
---certfile='~/analitico.ai.crt' \
---notebook-dir='~/notebooks'  \
+--keyfile='/home/www/analitico/analitico.ai.key' \
+--certfile='/home/www/analitico/analitico.ai.crt' \
+--notebook-dir='/home/www/analitico/notebooks'  \
 --NotebookApp.allow_origin='*' \
 --NotebookApp.token="$2"
 --NotebookApp.disable_check_xsrf=True &
