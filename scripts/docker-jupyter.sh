88483731201f927650d3fddc81be8847a2aa8e81
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-04-24 10:25:53 +0200

    copy sdk and s24 for jupyter

--- scripts/docker-jupyter.sh
+++ scripts/docker-jupyter.sh
@@ -3,7 +3,7 @@
 export LC_CTYPE=C.UTF-8
 cd /home/www/analitico/
 source venv/bin/activate
-export PYTHONPATH=/home/www/analitico/source/
+export PYTHONPATH=/home/www/analitico/libs/
 # start jupyter notebook
 echo "Start jupyter notebook" 
 exec jupyter notebook --port=8888 --ip=0.0.0.0 --no-browser \
