d01dc49fcb8ef64d159f6482eaa9755bd090b1bb
Gionata Mettifogo <gionata.mettifogo@analitico.ai>
2019-05-19 16:41:03 +0200

    Remove unused imports

--- source/api/urls.py
+++ source/api/urls.py
@@ -2,8 +2,7 @@ import os
 
 from django.urls import path
 from rest_framework import routers
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
