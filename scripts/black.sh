aeb62248b48952bb6c8bdbd11ae82bfa700e66e1
Gionata Mettifogo <gionata.mettifogo@analitico.ai>
2019-05-31 11:45:35 +0200

    Black: add serverless directory

--- scripts/black.sh
+++ scripts/black.sh
@@ -1,4 +1,4 @@
 # reformat source code using black at 120 chars per line
 # https://github.com/ambv/black
 cd ~/analitico/source
-black *.py */*.py */*/*.py */*/*/*.py -l 120
+black *.py */*.py */*/*.py */*/*/*.py ../serverless/*/*/*.py -l 120
