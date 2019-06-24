c5a27e87f95667c6cc62b1e63d369b7f5524022c
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-06-24 12:38:48 +0200

    validate response using json schema

--- serverless/templates/monitor/server.js
+++ serverless/templates/monitor/server.js
@@ -10,6 +10,8 @@ const express = require("express");
 const cron = require("node-cron");
 const fs = require("fs");
 const request = require("request");
+const Validator = require("jsonschema").Validator;
+const validator = new Validator();
 const Slack = require("slack-node");
 
 const slack = new Slack();
@@ -64,10 +66,12 @@ function getCronFunction(taskConfig) {
         const method = taskConfig.method || "GET";
         const data = taskConfig.data;
         const authorization = taskConfig.authorization;
-        const validation = taskConfig.validation;
+        const schema = taskConfig.schema;
+        const requestMaxTime = taskConfig.requestMaxTime || 5000;
         const payload = {
             method: method,
-            uri: url
+            uri: url,
+            time: true
         };
         if (method === "POST") {
             payload.json = data;
@@ -77,69 +81,82 @@ function getCronFunction(taskConfig) {
                 "bearer": authorization.bearerToken
             };
         }
-        request(payload, function (error, response, body) {
-            try {
-                if (error) {
-                    throw error;
+        try {
+            request(payload, function (error, response, body) {
+                try {
+                    if (error) {
+                        throw error;
+                    }
+
+                    if (response.elapsedTime > requestMaxTime) {
+                        notifyOnSlack(`${name}: exceeded max time ${response.elapsedTime}ms (max time: ${requestMaxTime}ms)`);
+                    }
+
+                    const result = validator.validate(body, schema);
+                    if (!result.valid) {
+                        const errors = result.errors;
+                        notifyOnSlack(`${name}: ${errors}`);
+                    }
+                    else {
+                        console.log(`${name}: ok`)
+                    }
                 }
-                console.log(body)
-                const result = validation(body);
-                if(!result.isValid){
-                    notifyOnSlack(`${name}: ${result.error}`);
+                catch (error) {
+                    notifyOnSlack(`${name}: ${error}`);
                 }
+            })
+        }
+        catch (error) {
+                notifyOnSlack(`${name}: request errror ${error}`);
             }
-            catch (error) {
-                notifyOnSlack(`${name} error ${error}`);
-            }
-        })
+        }
+}
+
+    function setupMonitoring() {
+        getConfiguration()
+            .then((configuration) => {
+                // destroy all current tasks
+                destroyAllTasks();
+                // for each configuration, create a cron
+                const tasks = configuration.tasks;
+                console.log(`Configuring ${tasks.length} tasks...`);
+                tasks.forEach(taskConfig => {
+                    const name = taskConfig.name;
+                    const schedule = taskConfig.schedule;
+
+
+                    if (monitoringTasksDict[name]) {
+                        console.error("Duplicate name");
+                        return true;
+                    }
+                    if (!cron.validate(schedule)) {
+                        console.error(`Invalid schedule ${schedule}`);
+                        return true;
+                    }
+                    const cronFunction = getCronFunction(taskConfig);
+                    cronFunction();
+                    const task = cron.schedule(schedule, cronFunction, { scheduled: true });
+                    monitoringTasks.push(task);
+                    monitoringTasksDict[name] = task;
+                });
+            })
+            .catch((e) => {
+                return console.error(e);
+            })
     }
-}
 
-function setupMonitoring() {
-    getConfiguration()
-        .then((configuration) => {
-            // destroy all current tasks
-            destroyAllTasks();
-            // for each configuration, create a cron
-            const tasks = configuration.tasks;
-            console.log(`Configuring ${tasks.length} tasks...`);
-            tasks.forEach(taskConfig => {
-                const name = taskConfig.name;
-                const schedule = taskConfig.schedule;
+    /**
+     * Send a slack notification
+     * @param {*} message 
+     */
+    function notifyOnSlack(message) {
+        return console.error(message);
+        slack.webhook({
+            channel: "#pingdom",
+            username: "monitor",
+            text: message
+        });
+    }
 
-
-                if (monitoringTasksDict[name]) {
-                    console.error("Duplicate name");
-                    return true;
-                }
-                if (!cron.validate(schedule)) {
-                    console.error(`Invalid schedule ${schedule}`);
-                    return true;
-                }
-                const cronFunction = getCronFunction(taskConfig);
-                cronFunction();
-                const task = cron.schedule(schedule, cronFunction, { scheduled: true });
-                monitoringTasks.push(task);
-                monitoringTasksDict[name] = task;
-            });
-        })
-        .catch((e) => {
-            return console.error(e);
-        })
-}
-
-/**
- * Send a slack notification
- * @param {*} message 
- */
-function notifyOnSlack(message) {
-    return console.error(message);
-    slack.webhook({
-        channel: "#pingdom",
-        username: "monitor",
-        text: message
-    });
-}
-
-// setup monitors
-setupMonitoring();
+    // setup monitors
+    setupMonitoring();
