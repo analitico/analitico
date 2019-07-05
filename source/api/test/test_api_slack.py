d5864cf5a5a09892dc5bc0633cc5d165c1dd7d8d
Gionata Mettifogo <gionata.mettifogo@analitico.ai>
2019-07-05 14:44:00 +0200

    api.slack.slack_send_internal_notification

--- source/api/test/test_api_slack.py
+++ source/api/test/test_api_slack.py
@@ -1,5 +1,6 @@
 import pytest
 import urllib
+import datetime
 
 from django.urls import reverse
 from rest_framework import status
@@ -64,3 +65,14 @@ class SlackTests(AnaliticoApiTestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["error"]["title"], "Slack returned error: invalid_code")
+
+    def test_slack_send_internal_notification(self):
+        api.slack.slack_send_internal_notification(
+            "This is a test on " + datetime.datetime.now().isoformat(),
+            "About a month after we started Y Combinator we came up with the phrase that became our motto: Make something people want. We've learned a lot since then, but if I were choosing now that's still the one I'd pick. "
+            "Another thing we tell founders is not to worry too much about the business model, at least at first. Not because making money is unimportant, but because it's so much easier than building something great. "
+            "A couple weeks ago I realized that if you put those two ideas together, you get something surprising. Make something people want. Don't worry too much about making money. What you've got is a description of a charity. "
+            "When you get an unexpected result like this, it could either be a bug or a new discovery. Either businesses aren't supposed to be like charities, and we've proven by reductio ad absurdum that one or both of the principles we began with is false. Or we have a new idea. "
+            "I suspect it's the latter, because as soon as this thought occurred to me, a whole bunch of other things fell into place.\n"
+            "http://www.paulgraham.com/good.html",
+        )
