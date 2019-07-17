c6dca133725b068f89224c54af206f298a2e446f
giovanni.zorzato <giovanni.zorzato@gmail.com>
2019-07-17 12:10:14 +0200

    do not keep error state if task is ok

--- serverless/templates/monitor/server.js
+++ serverless/templates/monitor/server.js
@@ -20,11 +20,7 @@ slack.setWebhook(slacWebhookUri);
 
 let monitoringTasks = [];
 let monitoringTasksDict = {};
-let status = {
-    status: "ok",
-    errors: [],
-    lastCheckAt: {}
-};
+let status = {};
 // App
 const app = express();
 
@@ -32,7 +28,8 @@ const app = express();
  * Health check: if we have collected some error, the status is 500
  */
 app.get("/", (req, res) => {
-    if (status.status !== "ok") {
+    const globalStatus = getGlobalStatus();
+    if (globalStatus === false) {
         res.status(500).send(status);
     }
     else {
@@ -40,20 +37,34 @@ app.get("/", (req, res) => {
     }
 });
 
-/**
- * Allows to reset the status
- */
-app.get("/reset", (req, res) => {
-    status = {
-        status: "ok",
-        errors: [],
-        lastCheckAt: {}
-    }
-    res.send(status);
-});
+
 
 app.listen(PORT, HOST);
 
+
+/**
+ * Initialize the status variable
+ */
+function initStatus() {
+    status = {
+        tasks: {}
+    };
+}
+
+/**
+ * Return false if one task is in error, otherwise returns true
+ */
+function getGlobalStatus(){
+    for (var taskKey in status.tasks) {
+        if (status.tasks.hasOwnProperty(taskKey)) {
+            let task = status.tasks[taskKey];
+            if(task.status === "error"){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 /**
  * Get monitoring configuration
@@ -96,51 +107,45 @@ function destroyAllTasks() {
  */
 function getCronFunction(taskConfig, options) {
     return () => {
-        let name = taskConfig.name;
-        const isStaging = options && options.staging;
-        if (isStaging) {
-            // add suffix
-            name = `${name}-staging`;
-        }
-        let url = taskConfig.url;
-        if (isStaging) {
-            // use staging url
-            url = taskConfig.stagingUrl;
-        }
-        const method = taskConfig.method || "GET";
-        const data = taskConfig.data;
-        const authorization = taskConfig.authorization;
-        const schema = taskConfig.schema;
-        const requestMaxTime = taskConfig.requestMaxTime || 5000;
-        const payload = {
-            method: method,
-            uri: url,
-            time: true,
-            json: true
-        };
-        if (method === "POST") {
-            payload.json = data;
-        }
-        if (authorization && authorization.bearerToken) {
-            payload.auth = {
-                "bearer": authorization.bearerToken
-            };
-        }
         try {
+            let name = taskConfig.name;
+            let url = taskConfig.url;
+            // do not trigger error on pingdom, only notify on slack
+            const onlyNotify = options && options.onlyNotify;
+            const method = taskConfig.method || "GET";
+            const data = taskConfig.data;
+            const authorization = taskConfig.authorization;
+            const schema = taskConfig.schema;
+            const requestMaxTime = taskConfig.requestMaxTime || 5000;
+            const payload = {
+                method: method,
+                uri: url,
+                time: true,
+                json: true
+            };
+            if (method === "POST") {
+                payload.json = data;
+            }
+            if (authorization && authorization.bearerToken) {
+                payload.auth = {
+                    "bearer": authorization.bearerToken
+                };
+            }
+
             request(payload, function (error, response, body) {
                 try {
                     if (error) {
                         throw error;
                     }
-                    status.lastCheckAt[name] = moment().format("YYYY-MM-DDTHH:mm:ssZZ");
+                    
 
                     // check status code
                     if (response.statusCode !== 200) {
-                        triggerError(`${name}: status code ${response.statusCode}, body ${body}`, taskConfig, isStaging);
+                        triggerError(`${name}: status code ${response.statusCode}, body ${body}`, taskConfig, onlyNotify);
                     }
                     // check if body is present
                     if (!body) {
-                        triggerError(`${name}: empty body!`, taskConfig, isStaging);
+                        triggerError(`${name}: empty body!`, taskConfig, onlyNotify);
                     }
 
                     if (response.timingPhases.total > requestMaxTime) {
@@ -152,22 +157,38 @@ function getCronFunction(taskConfig, options) {
                     if (!result.valid) {
                         const errors = result.errors;
                         // if it is staging, only notify without setting error
-                        triggerError(`${name}: ${errors}`, taskConfig, isStaging);
+                        triggerError(`${name}: ${errors}`, taskConfig, onlyNotify);
                     }
                     else {
                         if (process.env.DEBUG) {
                             console.log(`${name}: ok`)
                         }
+                        setTaskStatus(name, "ok", {
+                            lastCheckAt:  moment().format("YYYY-MM-DDTHH:mm:ssZZ"),
+                            requestTime: response.elapsedTime
+                        });
                     }
                 }
                 catch (error) {
-                    triggerError(`${name}: ${error}`, taskConfig, isStaging);
+                    triggerError(`${name}: ${error}`, taskConfig, onlyNotify);
                 }
             })
         }
         catch (error) {
-            triggerError(`${name}: request errror ${error}`);
+            triggerError(`${name}: request errror ${error}`, taskConfig);
         }
+    }
+}
+
+function setTaskStatus(name, taskStatus, dict){
+    if(typeof status.tasks[name] === "undefined"){
+        status.tasks[name] = {};
+    }
+    status.tasks[name].status = taskStatus;
+
+    if(dict){
+        // copy other properties
+        Object.assign(status.tasks[name], dict);
     }
 }
 
@@ -175,6 +196,7 @@ function getCronFunction(taskConfig, options) {
  * Schedule all the tasks
  */
 function setupMonitoring() {
+    initStatus();
     getConfiguration()
         .then((configuration) => {
             // destroy all current tasks
@@ -201,15 +223,25 @@ function setupMonitoring() {
 
                 monitoringTasks.push(task);
                 monitoringTasksDict[name] = task;
+                setTaskStatus(name, "init");
 
                 if (taskConfig.stagingUrl) {
+                    // clone
+                    const stagingConfig = JSON.parse(JSON.stringify(taskConfig));
+
+                    // add suffix
+                    stagingConfig.name = `${stagingConfig.name}-staging`;
+                    // replace url
+                    stagingConfig.url = stagingConfig.stagingUrl;
+                   
                     // if it has a staging url, configure the staging check
-                    const cronStagingFunction = getCronFunction(taskConfig, { staging: true });
+                    const cronStagingFunction = getCronFunction(stagingConfig, { onlyNotify: true });
                     // execute immediately
                     cronStagingFunction();
                     const taskStaging = cron.schedule(schedule, cronStagingFunction, { scheduled: true });
                     monitoringTasks.push(taskStaging);
-                    monitoringTasksDict[`${name}-staging`] = taskStaging;
+                    monitoringTasksDict[stagingConfig.name] = taskStaging;
+                    setTaskStatus(stagingConfig.name, "init");
                 }
             });
         })
@@ -230,7 +262,10 @@ function triggerError(message, taskConfig, onlyNotify) {
     message = `${date}: ${message} ${taskConfig ? taskConfig.url : ""}`;
     console.error(message);
     if (!onlyNotify) {
-        setErrorStatus(message);
+        // set error
+        setTaskStatus(taskConfig.name, "error", {
+            lastError: message
+        })
     }
     notifyOnSlack(message);
 }
@@ -246,20 +281,11 @@ function notifyOnSlack(message) {
         username: "monitor",
         text: message
     }, function (err, response) {
-        if (err) {
-            setErrorStatus(err);
-        }
+
     });
 }
 
-/**
- * Set the error status
- * @param {*} error 
- */
-function setErrorStatus(error) {
-    status.status = "error";
-    status.errors = status.errors.concat(error);
-}
+
 
 // setup monitors
 setupMonitoring();
