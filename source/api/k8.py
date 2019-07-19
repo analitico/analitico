d289d5080338509b6f5d68323b625a0743234181
danielebriggi <daniele.briggi@analitico.ai>
2019-07-19 19:01:17 +0200

    Update job image with new analitico image name.

--- source/api/k8.py
+++ source/api/k8.py
@@ -341,7 +341,7 @@ def k8_jobs_create(item: ItemMixin, job_action: str = None, job_data: dict = Non
             msg = "k8_jobs_create - ANALITICO_COMMIT_SHA is not defined, will build using :latest docker image instead."
             logger.warning(msg)
         build_image_tag = f":{commit_sha}" if commit_sha else ":latest"
-        configs["build_image"] = f"eu.gcr.io/analitico-api/analitico-website{build_image_tag}"
+        configs["build_image"] = f"eu.gcr.io/analitico-api/analitico{build_image_tag}"
 
     if job_action == analitico.ACTION_RUN_AND_BUILD:
         configs["job_template"] = os.path.join(K8_JOB_TEMPLATE_DIR, "job-run-and-build-template.yaml")
