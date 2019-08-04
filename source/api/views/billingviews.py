1103e16ba347cb93da8a39693355782205ce4221
Gionata Mettifogo <gionata.mettifogo@analitico.ai>
2019-08-04 11:31:59 +0200

    Enable billing test webhook for stripe

---
+++ source/api/views/billingviews.py
@@ -0,0 +1,75 @@
+
+import simplejson as json
+
+from rest_framework.viewsets import ViewSet
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.decorators import action, api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+
+
+import analitico.utilities
+from api.views.k8viewsetmixin import get_namespace, get_kubctl_response, K8ViewSetMixin
+import api.utilities
+
+from analitico import AnaliticoException, logger
+from django.core.mail import send_mail
+from django.views.decorators.csrf import csrf_exempt
+
+class BillingViewSet(ViewSet):
+    """ APIs to enable subscriptions, payments, etc. """
+
+    # Defined in subclass, eg: api.models.Endpoint
+    item_class = None
+
+    # Defined in subclass, eg: EndpointSerializer
+    serializer_class = None
+
+    # All methods require prior authentication, no token, no access
+    permission_classes = None
+
+    # Default format for requests is json
+    format_kwarg = "json"
+
+    ##
+    ## Cluster information (these APIs do not require an item_id)
+    ##
+
+    # WTF? not taking aunauthenticated calls
+    @csrf_exempt
+    @action(methods=["get, post"], detail=False, url_name="stripe-webhook", url_path="stripe/webhook", permission_classes=(AllowAny,))
+    def stripe_webook(self, request):
+        """ Stripe will call us on this endpoint with events related to billing, sales, subscriptions, etc... """
+        
+        event = request.data
+        event_type = event["type"]
+
+        send_mail(
+                subject=f"Stripe webhook: {event_type}",
+                message=json.dumps(event, indent=4),
+                from_email="notifications@analitico.ai",
+                recipient_list=["gionata.mettifogo@analitico.ai"],
+                fail_silently=False,
+            )
+
+        return Response(status=status.HTTP_200_OK)
+
+
+@api_view()
+def stripe_webhook(request: Request) -> Response:
+
+    event = request.data
+    event_type = event["type"]
+    event_json = json.dumps(event, indent=4)
+    logger.info(f"stripe_webhook - {event_type}\n\n{event_json}")
+
+    send_mail(
+        subject=f"Stripe webhook: {event_type}",
+        message=json.dumps(event, indent=4),
+        from_email="notifications@analitico.ai",
+        recipient_list=["gionata.mettifogo@analitico.ai"],
+        fail_silently=False,
+    )
+
+    return Response(status=status.HTTP_200_OK)
