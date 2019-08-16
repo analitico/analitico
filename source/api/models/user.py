34c11bcf9e98e80b16878ba75375c059d43eeb58
Gionata Mettifogo <gionata.mettifogo@analitico.ai>
2019-08-16 16:16:42 +0200

    Force fields to bool value

--- source/api/models/user.py
+++ source/api/models/user.py
@@ -15,15 +15,29 @@ class User(ItemMixin, AbstractUser):
     """ User model. """
 
     username = None
+
     email = models.EmailField(_("email address"), unique=True)
 
+    # https://docs.djangoproject.com/en/2.2/topics/auth/customizing/#django.contrib.auth.models.CustomUser.EMAIL_FIELD
     USERNAME_FIELD = "email"
+
+    # https://docs.djangoproject.com/en/2.2/topics/auth/customizing/#django.contrib.auth.models.CustomUser.REQUIRED_FIELDS
     REQUIRED_FIELDS = []
 
     objects = UserManager()
 
     # Additional attributes (like profile information) are open ended and are stored as json
     attributes = jsonfield.JSONField(load_kwargs={"object_pairs_hook": collections.OrderedDict}, blank=True, null=True)
+
+    def save(self, **kwargs):
+        """ Force boolean field to have a value to work around some SQL driver bug. """
+        if self.is_staff == None:
+            self.is_staff = False
+        if self.is_superuser == None:
+            self.is_superuser = False
+        if self.is_active == None:
+            self.is_active = False
+        super().save(**kwargs)
 
     def __str__(self):
         return self.email
