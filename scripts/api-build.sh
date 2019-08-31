21c71bc3b4558021c1acfb9475a7d712a706065d
danielebriggi <daniele.briggi@analitico.ai>
2019-08-31 16:09:56 +0200

    Packages path.

--- scripts/api-build.sh
+++ scripts/api-build.sh
@@ -14,15 +14,12 @@ pip install -r requirements.txt
 # since python 3.6 the library is no longer compatible with the standard library
 pip uninstall -y enum34
 
-# unit tests require access to analitico package (import analitico)
-export PYTHONPATH=$PYTHONPATH:/opt/conda/lib/python3.7/site-packages:/home/www/analitico/source
-
 echo "Collect static"
 cd source
-python3 ./manage.py collectstatic --noinput
+./manage.py collectstatic --noinput
 
 echo "Run tests"
 # do not use docker for papermill unit tests (it is not supported on Gitlab CI)
-python3 ./manage.py test --exclude-tag=slow --exclude-tag=docker
+./manage.py test --exclude-tag=slow --exclude-tag=docker
 
 echo "Done"
