364fc77a166ca5cf9c3c172811eebaa04f9fc379
danielebriggi <brdaniele@gmail.com>
2020-02-04 10:23:42 +0100

    Fix build exception.

--- scripts/api-build.sh
+++ scripts/api-build.sh
@@ -12,17 +12,18 @@ cd /home/www/analitico
 echo "Installing requirements"
 pip install --upgrade pip
 pip install -r requirements.txt
-# since python 3.6 the library is no longer compatible with the standard library.
-# it should be fixed with the ticket 
-# server / clean up and update requirements #143
-PYTHONPATH="" pip uninstall -y enum34
+# since python 3.6 the `enum34` is no longer compatible with the standard library.
+# it should be fixed with the ticket `server / clean up and update requirements #143`
+# `typing` library raises exception when collecting Django statics:
+# `AttributeError: type object 'Callable' has no attribute '_abc_registry'`
+PYTHONPATH="" pip uninstall -y enum34 typing
 
 echo "Collect static"
 cd source
-./manage.py collectstatic --noinput
+python3 ./manage.py collectstatic --noinput
 
 echo "Run tests"
 # do not use docker for papermill unit tests (it is not supported on Gitlab CI)
-./manage.py test --exclude-tag=slow --exclude-tag=docker
+python3 ./manage.py test --exclude-tag=slow --exclude-tag=docker
 
 echo "Done"
