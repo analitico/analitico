fc7e6e7b5334da9e3b7d38a3eca98c798cc8c9b9
danielebriggi <brdaniele@gmail.com>
2020-04-20 17:05:28 +0200

    Log response time in serverless Gunicorn.

--- serverless/templates/analitico-client/tasks/serverless-start.sh
+++ serverless/templates/analitico-client/tasks/serverless-start.sh
@@ -17,10 +17,12 @@ echo "Start gunicorn"
 # Do not use threads because we don't know which libraries
 # are run and thus they cant be not thread-safe. 
 # Increase timeout to give more freedom to the user's endpoint.
+# Custom access log format to add response time and X-forwarded-for host.
 exec gunicorn \
     --bind :$PORT \
     --workers 1 \
     --access-logfile - \
+    --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s %({content-length}i)s %(L)s "%(f)s" "%(a)s" "%({x-forwarded-for}i)s"' \
     --log-level debug \
     --timeout 60 \
     app:app
